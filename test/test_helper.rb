904ced3f30b8012e3cde46ad3eb958aef5906df2
cristtian177 <cristtian177@gmail.com>
2023-09-22 01:46:00 -0500

    [add] error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,6 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-
-require 'minitest/reporters'
-
-Minitest::Reporters.use! Minitest::Reporters::JUnitReporter.new
-
-
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
