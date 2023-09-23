803f9dc64437c04dc558eac062bd8e03a9540714
cristtian177 <cristtian177@gmail.com>
2023-09-23 02:35:54 -0500

    [add]

--- app/controllers/calculadora_controller.rb
+++ app/controllers/calculadora_controller.rb
@@ -1,5 +1,5 @@
 class CalculadoraController < ApplicationController
   def sumar
-    @resultado = params[:numero1].to_i - params[:numero2].to_i 
+    @resultado = params[:numero1].to_i + params[:numero2].to_i 
   end
 end
